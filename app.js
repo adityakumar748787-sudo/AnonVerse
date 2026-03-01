939c1880eebcf879a6ac532d8f5df373ca087a24
Aditya Kumar <adityakumar748787@gmail.com>
2026-03-01 21:53:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,4 @@ app.use(require("./routes/authRoutes"));
 app.use(require("./routes/postRoutes"));
 app.use(require("./routes/commentRoutes"));
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+module.exports = app;
